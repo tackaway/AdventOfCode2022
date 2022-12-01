51fa098ac95cb6f87371bf3ca994c36d4ccc9eee
tackaway <47019357+tackaway@users.noreply.github.com>
2022-12-01 12:50:20 -0500

    Initial commit

---
+++ Day1/AoC1.py
@@ -0,0 +1,13 @@
+elf, y, pos, val= 0,0,0,0    #Define Variables and Array
+elfs = []
+f = open("input.txt", "r")   #Open the file
+for x in f:                  #Iterate through each line
+    if(x=="\n"):             #Check for blank line
+        elfs.append(elf)     #Add total calories to elf array
+        elf=0                #Reset elf counter
+    else:
+        elf+=int(x)          #Add each elf calories 
+print("Value of highest Elf: " + str(max(elfs)))                 #Find max of Elfs
+print("Position of highest Elf: " + str(elfs.index(max(elfs))))  #Find position of Elfs
+elfs = sorted(elfs, reverse=True)     #Sort array
+print("The top 3 elfs are carrying " + str(elfs[0]+elfs[1]+elfs[2]) + " calories") #Print top 3 array results
